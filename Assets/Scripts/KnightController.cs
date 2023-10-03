a67e319d9cd2a63477b6326642df22fda4a7e3b7
FunTom <vogeltanz@utb.cz>
2023-10-03 17:36:36 +0200

    Knight Controller Script and Animation settings

---
+++ Assets/Scripts/KnightController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class KnightController : MonoBehaviour
+{
+
+    Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire"))
+        {
+            _animator.SetTrigger("Attack");
+        }
+
+
+        float vx = Input.GetAxisRaw("Horizontal");
+
+        if (vx != 0)
+        {
+            _animator.SetBool("Moving", true);
+        }
+        else
+        {
+            _animator.SetBool("Moving", false);
+        }
+    }
+}

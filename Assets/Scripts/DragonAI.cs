faa5999c0d2d178a80dca5738e46d3907c8c4a9f
FunTom <vogeltanz@utb.cz>
2023-10-24 17:35:45 +0200

    Dragon AI + importing new assets

---
+++ Assets/Scripts/DragonAI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class DragonAI : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float distanceToAttack = 1;
+
+    [SerializeField]
+    float speed = 2;
+
+    Animator _animator;
+    Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    bool isAttacking = false;
+    float direction;
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            float distance = target.position.x - this.transform.position.x;
+            direction = Mathf.Sign(distance);
+            
+            if (Mathf.Abs(distance) < distanceToAttack && isAttacking == false)
+            {
+                //attack
+                _animator.SetTrigger("Attack");
+                isAttacking = true;
+
+                //stop moving
+                _rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
+                _animator.SetBool("Moving", false);
+            }
+            else if(isAttacking == false)
+            {
+                //start moving
+                _rigidbody2D.velocity = new Vector2(direction * speed, _rigidbody2D.velocity.y);
+                _animator.SetBool("Moving", true);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.localScale.x > 0
+            && direction < 0
+            ||
+            transform.localScale.x < 0
+            && direction > 0)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+    }
+
+    public void StopAttacking()
+    {
+        isAttacking = false;
+    }
+}
